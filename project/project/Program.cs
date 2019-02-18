f74252b33ca895232743e7541dff6d65ce4f91fb
Péter Z <peter@z.hu>
2019-02-18 15:02:40 +0100

    asd

--- project/project/Program.cs
+++ project/project/Program.cs
@@ -10,7 +10,7 @@ namespace project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello wrold!");
+            Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
     }
