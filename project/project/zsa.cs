2e4b378682e26fd022b283e4f650d45bb073598b
szakkora1 <zsadam@g.hu>
2019-02-18 15:21:33 +0100

    vmi3

--- project/project/zsa.cs
+++ project/project/zsa.cs
@@ -12,5 +12,11 @@ namespace project
         int szam;
         int a = 1;
         int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
     }
 }
