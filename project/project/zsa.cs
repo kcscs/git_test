7d8ce4898a51c45665626a330b5638bbb8c7d126
szakkora1 <zsadam@g.hu>
2019-02-18 15:14:36 +0100

    vmi

---
+++ project/project/zsa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class zsa
+    {
+        string vmi;
+        int szam;
+        int a = 1;
+        int b = 2;
+    }
+}
